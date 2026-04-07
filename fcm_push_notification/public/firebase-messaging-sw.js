56872a39c4bb7429a9836d7bba6c6c80e89ee3de
PhanithVatanak <phanith.vatanak@gmail.com>
2026-04-07 08:53:54 +0000

    update sw click

--- fcm_push_notification/public/firebase-messaging-sw.js
+++ fcm_push_notification/public/firebase-messaging-sw.js
@@ -43,32 +43,29 @@ async function initFirebaseSW() {
 
 initFirebaseSW();
 
-// Notification Click Logic
+
 self.addEventListener("notificationclick", function(event) {
     event.notification.close();
 
     const data = event.notification.data || {};
-    let url = "/app";
+    let url = data.click_action || "/";
 
-    // Build Frappe URL: /app/doctype/docname
     if (data.doctype && data.docname) {
-        const doctype_slug = data.doctype.toLowerCase().replace(/ /g, '-');
-        url = `${self.location.origin}/app/${doctype_slug}/${data.docname}`;
-    } 
+        const doctype_lower = data.doctype.toLowerCase();
+        url = self.location.origin + "/app/" + doctype_lower + "/" + data.docname;
+    }
     else if (data.click_action) {
         url = data.click_action;
     }
-    
+
     event.waitUntil(
         clients.matchAll({ type: "window", includeUncontrolled: true })
             .then(windowClients => {
-                // If a tab is already open with this URL, focus it
                 for (let client of windowClients) {
-                    if (client.url === url && "focus" in client) {
+                    if (client.url.startsWith(self.location.origin + url) && "focus" in client) {
                         return client.focus();
                     }
                 }
-                // Otherwise, open a new window
                 if (clients.openWindow) {
                     return clients.openWindow(url);
                 }
